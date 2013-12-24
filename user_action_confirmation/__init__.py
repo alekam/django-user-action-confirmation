231dfb3d6114447a42794403e006ca205cbeff3a
Alex Kamedov <alex@kamedov.ru>
2013-12-24 13:23:26 +0600

    improve

--- user_action_confirmation/__init__.py
+++ user_action_confirmation/__init__.py
@@ -7,3 +7,8 @@ def get_version():
     if VERSION[2]:
         version = '%s.%s' % (version, VERSION[2])
     return version
+
+
+def create_ticket(action, user=None):
+    from user_action_confirmation.models import Confirmation
+    return Confirmation.objects.create(user=user, action=action).ticket
